93309257a8f782fecee9c43e333a2b2ee115ced0
Elufere Innocent <eluferedonatus@gmail.com>
2024-06-10 12:12:41 +0100

    commit

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,12 +1,32 @@
-import React from 'react';
-import "./login.scss";
+import React from "react";
+import "./login.scss"
 
 export function Login() {
-    
 
-    return (
-        <div className='login'>
-            login
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Hello World.</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero cum,
+            alias totam numquam ipsa exercitationem dignissimos, error nam,
+            consequatur.
+          </p>
+          <span>Don't you have an account?</span>
+            <button>Register</button>
         </div>
-    )
-}
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
