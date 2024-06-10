675f52212c9171c2f0b9d41b129bb8fbc0c91616
Elufere Innocent <eluferedonatus@gmail.com>
2024-06-10 11:11:48 +0100

    commit

---
+++ src/pages/profile/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./profile.scss";
+
+export function Profile() {
+    
+
+    return (
+        <div className='profile'>
+            profile
+        </div>
+    )
+}
